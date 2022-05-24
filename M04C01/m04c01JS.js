36f64a2cf1a29a732fa1308af471f236a4421340
Guillermo Malaga <gmg.proyectos.2013@gmail.com>
2022-05-24 11:01:10 -0400

    sd

--- M04C01/m04c01JS.js
+++ M04C01/m04c01JS.js
@@ -13,3 +13,51 @@ if(palabra.toUpperCase().startsWith("A")){
 }else{
     console.log("No");
 }
+
+//convierte en mayusculas la palabra original
+palabra = palabra.toUpperCase();
+//this hace referencia a valores o funciones dentro del mismo objeto
+function ejecutar(){
+    console.log("fuera");
+}
+
+var objeto={
+    ejecutar:function(){
+        console.log("dentro");
+    },
+    comprobar:function(){
+        this.ejecutar();
+    }
+}
+
+objeto.comprobar();
+
+//---------------------------------------funcioes flecha
+//funcion tradicional
+function escribir(texto){
+    console.log(texto);
+}
+//funcion flecha
+var f=texto=>console.log(texto);
+
+f("hola");
+
+function saludar(){
+    console.log("hola como estas");
+}
+saludar();
+
+f=()=> console.log("hola cxomo estas");
+
+//ejemplo4
+
+function mostrarPersona(nombre,apellido){
+    console.log(nombre+":"+apellido);
+}
+
+let m=(n,a)=>console.log(n+":"+a);
+m("alejandra","ruiz");
+
+//construccion de una funcion flecha
+
+//(par1,par2,....)=>{intrucion1,instrucion2}
