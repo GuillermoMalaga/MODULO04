3cd40f9526111c294e913086fe56bb1e1756d24e
Guillermo Malaga <gmg.proyectos.2013@gmail.com>
2022-06-01 17:22:21 -0400

    asincrono clase3 terminado

---
+++ M04C01/Asincronico01/AsincJS.js
@@ -0,0 +1,9 @@
+ //creando la clase item
+ class item{
+     constructor(nuevaTarea){
+         this.createDiv(nuevaTarea);
+     }
+     createDiv=function(tarea){
+        //aqui va el codigo que agrag la nueva tarea 
+    }
+ }
